f4c92a68221e5e80c07d46c01dee2454eef99c35
Florent Ghilain <florent.ghilain@cikisi.com>
2020-03-22 20:17:08 +0100

    add test files
    - add pytest mode in main.py (using test config file)
    
    - add tests using pytest/pytest-aiohttp

--- covid_api/main.py
+++ covid_api/main.py
@@ -6,10 +6,15 @@ from covid_api.db import close_pg, init_pg
 from covid_api.routes import setup_routes, setup_cors
 from covid_api.helpers import load_config
 
-def init_app():
+def init_app(pytest=False):
   app = web.Application()
-  app['db_config'] = load_config('config/database.yaml')
-  app['http_config'] = load_config('config/server.yaml')
+
+  if pytest:
+    app['db_config'] = load_config('config/test-database.yaml')
+    app['http_config'] = load_config('config/test-server.yaml')
+  else:
+    app['db_config'] = load_config('config/database.yaml')
+    app['http_config'] = load_config('config/server.yaml')
 
   # create db connection on startup, shutdown on exit
   app.on_startup.append(init_pg)
---
+++ tests/test_basics.py
@@ -0,0 +1,13 @@
+
+async def test_index(client, db):
+  case = { 'national_id': 1, 'country': 'Belgium', 'age': 54, 'health': 'infected' }
+  response = await client.post('/api/add', json=case)
+
+  assert response.status == 200
+
+  response = await client.get('/api/list?size=1&sort=desc')
+  data = await response.json()
+
+  assert response.status == 200
+  assert len(data) == 1
+  assert case == data[0]
